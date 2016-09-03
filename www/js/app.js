c649cab731fd9d0466b0ff2669c8a32d3b16998d
GrokSoft <bill@groksoft.com>
2016-09-03 12:31:22 -0600

    More jQuery to angular fixes. Still need to fix getWidth()

--- www/js/app.js
+++ www/js/app.js
@@ -169,6 +169,83 @@
 
     }]);
 
+    /**
+     * Horizontal & Vertical collapse
+     */
+    app.directive('collapseWidthHeight', ['$transition', '$timeout', function ($transition, $timeout) {
+
+        return {
+            link: function (scope, element, attrs) {
+
+                var initialAnimSkip = true;
+                var currentTransition;
+
+                function doTransition(change) {
+                    var newTransition = $transition(element, change);
+                    if (currentTransition) {
+                        currentTransition.cancel();
+                    }
+                    currentTransition = newTransition;
+                    newTransition.then(newTransitionDone, newTransitionDone);
+                    return newTransition;
+
+                    function newTransitionDone() {
+                        // Make sure it's this transition, otherwise, leave it alone.
+                        if (currentTransition === newTransition) {
+                            currentTransition = undefined;
+                        }
+                    }
+                }
+
+                function expand() {
+                    if (initialAnimSkip) {
+                        initialAnimSkip = false;
+                        expandDone();
+                    } else {
+                        element.removeClass('collapse').addClass('collapsing-width-height');
+                        doTransition({ width: element[0].scrollWidth + 'px',height: element[0].scrollHeight + 'px' }).then(expandDone);
+                    }
+                }
+
+                function expandDone() {
+                    element.removeClass('collapsing-width-height');
+                    element.addClass('collapse in');
+                    element.css({width: 'auto'});
+                    element.css({height: 'auto'});
+                }
+
+                function collapse() {
+                    if (initialAnimSkip) {
+                        initialAnimSkip = false;
+                        collapseDone();
+                        element.css({width: 0});
+                    } else {
+                        // CSS transitions don't work with height: auto, so we have to manually change the height to a specific value
+                        element.css({ width: element[0].scrollWidth + 'px', height: element[0].scrollHeight + 'px' });
+                        //trigger reflow so a browser realizes that height was updated from auto to a specific value
+                        var x = element[0].offsetHeight;
+
+                        element.removeClass('collapse in').addClass('collapsing-width-height');
+
+                        doTransition({ width: 0, height: 0 }).then(collapseDone);
+                    }
+                }
+
+                function collapseDone() {
+                    element.removeClass('collapsing-width-height');
+                    element.addClass('collapse');
+                }
+
+                scope.$watch(attrs.collapseWidthHeight, function (shouldCollapse) {
+                    if (shouldCollapse) {
+                        collapse();
+                    } else {
+                        expand();
+                    }
+                });
+            }
+        };
+    }]);
 
     /**
      * Horizontal collapse
@@ -246,6 +323,7 @@
             }
         };
     }]);
+
 
 
     /**
@@ -390,9 +468,13 @@
         $scope.isCollapsedList = false;
 
         $scope.getWidth = function (elem) {
-            var domElement = elem;
+            /*var domElement = elem;
             var theDiv      = angular.element(domElement);
-            return theDiv.width;
+            var w = domElement.clientWidth;
+            var w1 = theDiv.clientWidth;
+            var w3 = elem.clientWidth;*/
+            var divsize = angular.element(document.getElementById(elem)).prop('offsetWidth');
+            return w;
         }
 
     }]);
