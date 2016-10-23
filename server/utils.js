740e231e2c95dae55387e203a872b10e70a421ba
GrokSoft <bill@groksoft.com>
2016-10-22 23:36:40 -0600

    Added instructions to use http://loripsum.net/api

--- server/utils.js
+++ server/utils.js
@@ -73,6 +73,25 @@ function Utils() {
      *
      * NOTE: This function must come before the plexData object that is instantiated below
      *
+     * ToDo: Change this to use the api
+     *
+     * How do I use the API?
+         Just do a GET request on loripsum.net/api, to get some placeholder text. You can add extra parameters to specify the output you're going to get. Say, you need 10 short paragraphs with headings, use loripsum.net/api/10/short/headers. All of the possible parameters are:
+
+         (integer) - The number of paragraphs to generate.
+         short, medium, long, verylong - The average length of a paragraph.
+         decorate - Add bold, italic and marked text.
+         link - Add links.
+         ul - Add unordered lists.
+         ol - Add numbered lists.
+         dl - Add description lists.
+         bq - Add blockquotes.
+         code - Add code samples.
+         headers - Add headers.
+         allcaps - Use ALL CAPS.
+         prude - Prude version.
+         plaintext - Return plain text, no HTML.
+     *
      * @param start
      * @param len
      *
